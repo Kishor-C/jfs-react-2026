0a1af8c563d11ca5f01aedf27dd5a012e18ebcd9
Kishor C <kishor1124@gmail.com>
2026-02-13 07:51:02 +0530

    react examples

--- react-examples/my-contact-ui/src/App.jsx
+++ react-examples/my-contact-ui/src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Profile } from './components/Profile'
+import { Profile, SimpleArrays } from './components/Profile'
 
 
 function App() {
+  let profile1 = {id: 7890, name : "Alex1", dob: "2000-11-25"};
+  let profile2 = {id: 7896, name : "Alex2", dob: "2001-11-25"};
+  let profile3 = {id: 8890, name : "Alex3", dob: "2002-11-25"};
+  
+
+  
   return (
    <div>
      <h2>This is a root component</h2>
-     <Profile />
+     <Profile details = {profile1} />
+     <Profile details = {profile2} />
+     <Profile details = {profile3} />
+     <SimpleArrays />
    </div>
   )
 }
--- react-examples/my-contact-ui/src/components/Profile.jsx
+++ react-examples/my-contact-ui/src/components/Profile.jsx
@@ -1,9 +1,17 @@
 
-export function Profile() {
-    let id = 12345;
-    let name = "Alex";
-    let dob = "1999-10-15";
+export function Profile(props) {  
+    let id = props.details.id;
+    let name = props.details.name;
+    let dob = props.details.dob;
     return (<div>
         <h2>Hello {name}, id is {id} and your dob is {dob}</h2>
+    </div>)
+}
+
+export function SimpleArrays(props) {
+    let names = ["Alex", "Brad", "Charles", "David"];
+    return (<div>
+        <h2>Simple Arrays</h2>
+        {names.map((item, index) => <p key = {item}>{index + 1}. {item}</p>)}
     </div>)
 }
