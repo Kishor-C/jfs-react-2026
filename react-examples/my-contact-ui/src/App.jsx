77f58c15acd2e1fe5b4762e3c48a564fd147cc87
Kishor C <kishor1124@gmail.com>
2026-02-17 07:49:06 +0530

    react examples

--- react-examples/my-contact-ui/src/App.jsx
+++ react-examples/my-contact-ui/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Profile, SimpleArrays } from './components/Profile'
+import { Profile, UserForm } from './components/Profile'
 
 
 function App() {
@@ -10,15 +10,15 @@ function App() {
   let profile2 = {id: 7896, name : "Alex2", dob: "2001-11-25"};
   let profile3 = {id: 8890, name : "Alex3", dob: "2002-11-25"};
   
-
+  let profiles = [profile1, profile2, profile3];
   
   return (
-   <div>
-     <h2>This is a root component</h2>
-     <Profile details = {profile1} />
-     <Profile details = {profile2} />
-     <Profile details = {profile3} />
-     <SimpleArrays />
+   <div className="container-fluid">
+     <h2 className="text-center">This is a root component</h2>
+     {profiles.map((profile, index)=><Profile key = {profile.id} details = {profile}/>)}
+     <div className="w-50">
+      <UserForm />
+     </div>
    </div>
   )
 }
--- react-examples/my-contact-ui/src/components/Profile.jsx
+++ react-examples/my-contact-ui/src/components/Profile.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 export function Profile(props) {  
     let id = props.details.id;
@@ -7,11 +8,29 @@ export function Profile(props) {
         <h2>Hello {name}, id is {id} and your dob is {dob}</h2>
     </div>)
 }
-
-export function SimpleArrays(props) {
-    let names = ["Alex", "Brad", "Charles", "David"];
+export function UserForm() {
+    //useState(initialValue)
+    //console.log("rendering userForm()")
+    let [name, setName] = useState("Guest");
+    let [phone, setPhone] = useState("0");
+    
+    let handleSubmit = (e) => {
+        // prevent refresh
+        e.preventDefault();
+        console.log(name, phone);
+    }
+    
     return (<div>
-        <h2>Simple Arrays</h2>
-        {names.map((item, index) => <p key = {item}>{index + 1}. {item}</p>)}
+        <h2 className="alert alert-primary w-50 text-center">User component</h2>
+        <form onSubmit = {handleSubmit}>
+            Name: <input className="form-control form-control-lg w-50" autoComplete="off" 
+                type = "text" name = "username" onChange = {(e)=>setName(e.target.value)}/> <br />
+            Phone: <input className="form-control form-control-lg w-50"  autoComplete="off" 
+                type = "number" name = "userphone" onChange =  {(e)=>setPhone(e.target.value)}/> <br />
+            <input className="btn btn-primary btn-lg w-50" type = "submit" value = "Save" />
+        </form>
+        <div>
+            Name = {name}, Phone = {phone}
+        </div>
     </div>)
 }
